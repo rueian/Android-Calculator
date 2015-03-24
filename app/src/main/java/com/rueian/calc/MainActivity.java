7f811e2741fec899af90c23d5925b1eafac06dc1
Rueian Oneecom <rueiancsie@gmail.com>
2015-03-25 01:59:39 +0800

    basic view

--- app/src/main/java/com/rueian/calc/MainActivity.java
+++ app/src/main/java/com/rueian/calc/MainActivity.java
@@ -4,16 +4,26 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
 
+    protected EditText inputField;
+    protected EditText resultField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-    }
 
+        inputField = (EditText)findViewById(R.id.input);
+        resultField= (EditText)findViewById(R.id.result);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +45,10 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onBtnClick(View view) {
+
+        inputField.append(((Button)view).getText());
     }
 }

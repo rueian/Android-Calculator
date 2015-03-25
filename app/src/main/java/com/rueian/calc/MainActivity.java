0949f3f7eb0a9ce3707c0527f24d175131c11b01
Rueian Oneecom <rueiancsie@gmail.com>
2015-03-25 20:43:44 +0800

    remove menu import

--- app/src/main/java/com/rueian/calc/MainActivity.java
+++ app/src/main/java/com/rueian/calc/MainActivity.java
@@ -9,8 +9,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
